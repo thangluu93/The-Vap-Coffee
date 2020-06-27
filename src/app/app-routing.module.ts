0912f0725b2bbe3b66c6bb4c0d53ce08dfc12e17
duc <hohuynhduca7bh123@gmail.com>
2020-06-27 10:08:35 +0700

    fix design look  like better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{
-   path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) }, 
-{ path: 'login', loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule) },
-   { path: 'makeline', loadChildren: () => import('./pages/make-line/make-line.module').then(m => m.MakeLineModule) },];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+  {path: 'home', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule)},
+  {path: 'login', loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule) },
+  {path: 'makeline', loadChildren: () => import('./pages/make-line/make-line.module').then(m => m.MakeLineModule) },
+  { path: 'Menu', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
