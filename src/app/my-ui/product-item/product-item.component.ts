2804591680d6e68da949febb83a0f1fa2a3ca937
duc <hohuynhduca7bh123@gmail.com>
2020-06-29 00:04:59 +0700

    makeline, homepage,....

--- src/app/my-ui/product-item/product-item.component.ts
+++ src/app/my-ui/product-item/product-item.component.ts
@@ -21,4 +21,5 @@ export class ProductItemComponent implements OnInit {
     this.msg.sendMsg(this.productItem);
   }
 
+
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,9 +10,9 @@ export class ProductService {
 
   products: Product[] = [
     new Product(1, 'Cà phê sữa', 25),
-    new Product(2, 'Espresso', 29), 
-    new Product(3, 'Caramel Machiato', 55), 
-    new Product(4, 'Capuchino', 45), 
+    new Product(2, 'Espresso', 29),
+    new Product(3, 'Caramel Machiato', 55),
+    new Product(4, 'Capuchino', 45),
     new Product(5, 'Americano', 50),
 
 
@@ -25,6 +25,6 @@ export class ProductService {
   }
 
 
-  
+
 
 }
