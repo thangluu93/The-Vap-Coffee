83277e6423caa3ebe220f81685b39fc3e7b99751
duc <hohuynhduca7bh123@gmail.com>
2020-06-30 21:43:23 +0700

    home background

--- src/app/pages/make-line/make-line.module.ts
+++ src/app/pages/make-line/make-line.module.ts
@@ -10,7 +10,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [MakeLineComponent],
   imports: [
@@ -20,6 +20,7 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatButtonModule,
     MyUiModule
   ],
   providers: [MakeLineService]
